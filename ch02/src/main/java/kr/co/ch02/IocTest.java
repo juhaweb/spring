eabfd81907b811bee645dc6c1b9d1ffed85f2bec
juhaweb <u66kie@naver.com>
2020-01-06 15:41:26 +0900

    2020/01/06

---
+++ ch02/src/main/java/kr/co/ch02/IocTest.java
@@ -0,0 +1,45 @@
+package kr.co.ch02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/*
+ * 날짜 : 2020/01/06
+ * 이름 : 하정욱
+ * 내용 : Ioc 실습하기
+ */
+
+public class IocTest {
+
+	public static void main(String[] args) {
+		
+		// Ioc 적용 X 객체생성
+		TV ltv = new LgTV();
+		
+		ltv.power();
+		ltv.chUp();
+		ltv.soundUp();
+		
+		TV stv = new SamsungTV();
+		stv.power();
+		stv.chUp();
+		stv.soundUp();
+		
+		
+		// Spring Ioc 적용  (Ioc컨테이너 -> 스프링 컨테이너라고 많이 함)
+		ApplicationContext ctx = new GenericXmlApplicationContext("spring-context.xml");
+		TV tv1 = (LgTV) ctx.getBean("ltv");
+		TV tv2 = (SamsungTV) ctx.getBean("stv");
+		
+		tv1.power();
+		tv1.chUp();
+		tv1.soundUp();
+		
+		tv2.power();
+		tv2.chUp();
+		tv2.soundUp();
+		
+		
+	}
+	
+}
---
+++ ch02/src/main/java/kr/co/ch02/sub1/DiSetterTest.java
@@ -0,0 +1,34 @@
+package kr.co.ch02.sub1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import kr.co.ch02.LgTV;
+import kr.co.ch02.SamsungTV;
+import kr.co.ch02.TV;
+
+/*
+ * 날짜 : 2020/01/06
+ * 이름 : 하정욱
+ * 내용 : DI Setter 구현 실습하기
+ */
+
+public class DiSetterTest {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new GenericXmlApplicationContext("spring-context.xml");
+		TV tv1 = (LgTV) ctx.getBean("tv1");
+		TV tv2 = (SamsungTV) ctx.getBean("tv2");
+		
+		tv1.power();
+		tv1.chUp();
+		tv1.soundUp();
+		
+		tv2.power();
+		tv2.chUp();
+		tv2.soundUp();
+		
+		
+	}
+}
