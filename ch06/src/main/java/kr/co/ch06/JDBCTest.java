13adff71894abe0a12be61ad30cc759ed3850343
juhaweb <u66kie@naver.com>
2020-01-13 10:11:19 +0900

    2020/01/13

---
+++ ch06/src/main/java/kr/co/ch06/JDBCTest.java
@@ -0,0 +1,24 @@
+package kr.co.ch06;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+/*
+ * 날짜 : 2020/01/13
+ * 이름 : 하정욱
+ * 내용 : 스프링 JDBC 실습하기
+ */
+public class JDBCTest {
+
+	public static void main(String[] args) {
+		
+		// 컨넥션 풀이니까 db 계정 아이디 비번 정해져있어야함. 쿼리문 실행할때 접속하는 방식이 아님 --> context.xml 에 컨넥션풀 설정을 해야함. 
+		ApplicationContext ctx = new GenericApplicationContext("spring-context.xml");
+		
+		
+		
+		
+		
+	}
+	
+}
