806389acb631c60fa7fc458a7c0181602bb9d773
juhaweb <u66kie@naver.com>
2020-01-14 10:13:00 +0900

    2020/01/14 MVC 초기설정 완료

---
+++ ch07/src/main/java/kr/co/ch07/controller/MainController.java
@@ -0,0 +1,21 @@
+package kr.co.ch07.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";	
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+	
+
+}
